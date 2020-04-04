dbd5db9bd73edb960f896b6345245a5e7ff285ac
AdRusinek <rusinekadrian19@gmail.com>
2020-04-04 03:43:23 +0200

    Adding documenting validation constraints asciidoc

--- target/generated-sources/annotations/com/rusinek/msscrestdocsexample/web/mappers/BeerMapperImpl.java
+++ target/generated-sources/annotations/com/rusinek/msscrestdocsexample/web/mappers/BeerMapperImpl.java
@@ -11,7 +11,7 @@ import org.springframework.stereotype.Component;
 
 @Generated(
     value = "org.mapstruct.ap.MappingProcessor",
-    date = "2020-04-04T03:19:18+0200",
+    date = "2020-04-04T03:41:41+0200",
     comments = "version: 1.3.0.Final, compiler: javac, environment: Java 1.8.0_181 (Oracle Corporation)"
 )
 @Component
